eb9c43bcc306ed20808bb86bb2dccf06f003db23
Ng Bob Shoaun <ngbobshoaun2000@gmail.com>
2018-05-17 16:28:50 +0000

    first commit

---
+++ Assets/Scripts/Cop.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cop : MonoBehaviour {
+
+	public Transform head;
+	public Transform ufo;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//head.LookAt (ufo.position, -head.forward);
+		head.rotation = Quaternion.Euler(new Vector3 (0, 0, Quaternion.LookRotation (ufo.position).eulerAngles.x));
+		//head.rotation = Quaternion.LookRotation (ufo.position, Vector3.up);
+	}
+
+}
---
+++ Assets/Scripts/UFO.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UFO : MonoBehaviour {
+
+	private Rigidbody2D rb;
+	public float speed;
+	public float suckStrength;
+	private bool inputDetected;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		inputDetected = Input.GetAxis ("Horizontal") > 0 ||  Input.GetAxis ("Horizontal") < 0;
+
+		if (Input.GetKey (KeyCode.A)) {
+			//rb.AddForce (Vector2.left * speed, ForceMode2D.Force);
+		}
+		if (Input.GetKeyDown (KeyCode.A)) {
+		//	rb.AddTorque (-10);
+		}
+		if (inputDetected) {
+			//transform.Translate (Vector3.right * Input.GetAxis ("Horizontal"));
+			transform.position += Vector3.right * Input.GetAxis ("Horizontal");
+		}
+	}
+
+	private void FixedUpdate () {
+		if (inputDetected) {
+			//rb.AddForce (Vector2.right * Input.GetAxis("Horizontal") * speed, ForceMode2D.Force);
+			//rb.AddTorque (10);
+			//rb.MoveRotation (20);
+
+		}
+	}
+
+	void OnTriggerStay2D (Collider2D collider2D) {
+		Debug.Log ((transform.position - collider2D.transform.position).normalized);
+		collider2D.GetComponent<Rigidbody2D> ().AddForce ((transform.position - collider2D.transform.position) * suckStrength, ForceMode2D.Impulse);
+	}
+
+}
