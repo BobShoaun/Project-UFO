bf6680f537b622300fb890fcea9d9f91af4a3a67
Ng Bob Shoaun <ngbobshoaun2000@gmail.com>
2018-05-17 17:32:01 +0000

    ufo

--- Assets/Scripts/UFO.cs
+++ Assets/Scripts/UFO.cs
@@ -8,14 +8,27 @@ public class UFO : MonoBehaviour {
 	public float speed;
 	public float suckStrength;
 	private bool inputDetected;
+	public GameObject tractorBeam;
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D> ();
+		//test
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		inputDetected = Input.GetAxis ("Horizontal") > 0 ||  Input.GetAxis ("Horizontal") < 0;
+
+		if (inputDetected) {
+			//transform.Translate (Vector3.right * Input.GetAxis ("Horizontal"));
+			//transform.position += Vector3.right * Input.GetAxis ("Horizontal");
+			//rb.MovePosition (transform.position + Vector3.right * Input.GetAxis ("Horizontal"));
+			rb.velocity = Vector3.right * Input.GetAxis ("Horizontal") * speed;
+		}
+
+
+		tractorBeam.SetActive (Input.GetKey (KeyCode.Space));
 
 		if (Input.GetKey (KeyCode.A)) {
 			//rb.AddForce (Vector2.left * speed, ForceMode2D.Force);
@@ -23,10 +36,7 @@ public class UFO : MonoBehaviour {
 		if (Input.GetKeyDown (KeyCode.A)) {
 		//	rb.AddTorque (-10);
 		}
-		if (inputDetected) {
-			//transform.Translate (Vector3.right * Input.GetAxis ("Horizontal"));
-			transform.position += Vector3.right * Input.GetAxis ("Horizontal");
-		}
+		
 	}
 
 	private void FixedUpdate () {
@@ -39,8 +49,8 @@ public class UFO : MonoBehaviour {
 	}
 
 	void OnTriggerStay2D (Collider2D collider2D) {
-		Debug.Log ((transform.position - collider2D.transform.position).normalized);
-		collider2D.GetComponent<Rigidbody2D> ().AddForce ((transform.position - collider2D.transform.position) * suckStrength, ForceMode2D.Impulse);
+		//Debug.Log ((transform.position - collider2D.transform.position).normalized);
+		//collider2D.GetComponent<Rigidbody2D> ().AddForce ((transform.position - collider2D.transform.position) * suckStrength, ForceMode2D.Impulse);
 	}
 
 }
