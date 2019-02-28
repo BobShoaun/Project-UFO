762333d4e8ccc280ceff2b21dc0bd8a6345b66ef
Ng Bob Shoaun <ngbobshoaun2000@gmail.com>
2019-02-28 15:47:48 +0000

    No Comments

---
+++ Assets/Scripts/Bomb.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour {
+
+	// Use this for initialization
+	IEnumerator Start () {
+		yield return new WaitForSeconds (3);
+		 RaycastHit[] hits = null;
+	//	if (Physics2D.CircleCast (transform.position, 3, Vector2.left, 3, 0, hits) > 0) {
+
+	//	}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
